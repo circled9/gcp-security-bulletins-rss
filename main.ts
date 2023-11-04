02ee6031b19705b69876e3f5867f20f31a3a446c
Matsui Masashi <5251092+circled9@users.noreply.github.com>
2023-11-04 17:58:06 +0900

    create hono server

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
-import { parse } from "https://deno.land/x/xml@2.1.3/mod.ts";
+import { Hono } from "https://deno.land/x/hono/mod.ts";
+import { parse, stringify } from "https://deno.land/x/xml@2.1.3/mod.ts";
 
 const URL =
   "https://cloud.google.com/feeds/google-cloud-security-bulletins.xml" as const;
@@ -11,7 +12,7 @@ type Node = {
 } | Leaf;
 
 type Feed = {
-  xml: Node;
+  xml?: Node;
   feed: {
     id: string;
     title: string;
@@ -67,13 +68,16 @@ function reformat(feed: Feed): Feed {
   const { entry, updated, ...rest } = feed.feed;
   const updateUpdated = getUpdateUpdated(updated);
   return {
-    xml: feed.xml,
     feed: {
       ...rest,
       updated,
       entry: entry.map((feedItem) => {
         return {
           ...feedItem,
+          content: {
+            ...feedItem.content,
+            "#text": "<![CDATA[" + feedItem.content["#text"] + "]]>",
+          },
           updated: updateUpdated(feedItem),
         };
       }),
@@ -85,9 +89,20 @@ async function main() {
   const feed = await getFeed();
   const parsed = parseFeed(feed);
   const reformated = reformat(parsed);
-  console.log(reformated);
+
+  // HACK: stringifyするとdoctypeがおかしくなるので自前でdoctypeを付与する
+  const doctype = '<?xml version="1.0" encoding="UTF-8"?>';
+  return doctype + stringify(reformated);
 }
 
-if (import.meta.main) {
-  main();
-}
+const app = new Hono();
+
+app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
+
+app.get("/feeds/google-cloud-security-bulletins.xml", async (c) => {
+  const feed = await main();
+  c.header("Content-Type", "text/xml");
+  return c.body(feed);
+});
+
+Deno.serve(app.fetch);
